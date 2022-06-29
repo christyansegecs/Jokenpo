720a7045beaa49e1c68b137e15ab0d8e51db872e
christyansegecs <christyansegecs@hotmail.com>
2022-06-29 16:42:23 -0300

    Melhoria de Código

--- app/src/main/java/com/chris/jokenpo/MainActivity.kt
+++ app/src/main/java/com/chris/jokenpo/MainActivity.kt
@@ -2,104 +2,93 @@ package com.chris.jokenpo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val ivPadrao by lazy { findViewById<ImageView>(R.id.ivPadrao) }
+    private val ivPedra by lazy { findViewById<ImageView>(R.id.ivPedra) }
+    private val ivPapel by lazy { findViewById<ImageView>(R.id.ivPapel) }
+    private val ivTesoura by lazy { findViewById<ImageView>(R.id.ivTesoura) }
+    private val tvTitle by lazy { findViewById<TextView>(R.id.tvTitle) }
+    private val tvBody by lazy { findViewById<TextView>(R.id.tvBody) }
+    private var userChoose : Int? = null
+    private var cpuChoose : Int? = null
+    private var userChooseStr : String? = null
+    private var cpuChooseStr : String? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lateinit var randomStr : String
-        lateinit var resultado : String
-        lateinit var derrota : String
-        lateinit var vitoria : String
-        lateinit var empate : String
-        var randomInt : Int
+        setListeners()
 
-        randomInt = Random.nextInt(0,2)
+    }
 
-
-        val ivPadrao = findViewById<ImageView>(R.id.ivPadrao)
-        val ivPedra = findViewById<ImageView>(R.id.ivPedra)
-        var ivPapel = findViewById<ImageView>(R.id.ivPapel)
-        val ivTesoura = findViewById<ImageView>(R.id.ivTesoura)
-        val tvTitle = findViewById<TextView>(R.id.tvTitle)
-        val tvBody = findViewById<TextView>(R.id.tvBody)
-
-
-        derrota = "Você Perdeu =/"
-        vitoria = "Você Ganhou =]"
-        empate = "Empatou >.<"
-
-
-        if (randomInt == 0) {
-            randomStr = "Pedra"
-        }
-        else if (randomInt == 1) {
-            randomStr = "Papel"
-        }
-        else {
-            randomStr = "Tesoura"
-        }
-
+    private fun setListeners() {
         ivPedra.setOnClickListener{
-            if (randomStr == "Pedra") {
-                resultado = empate
-                ivPadrao.setImageResource((R.drawable.pedra))
-            }
-            if (randomStr == "Papel") {
-                resultado = derrota
-                ivPadrao.setImageResource((R.drawable.papel))
-            }
-            if (randomStr == "Tesoura") {
-                resultado = vitoria
-                ivPadrao.setImageResource((R.drawable.tesoura))
-            }
-            tvTitle.setText("O CPU escolheu ${randomStr}")
-            tvBody.setText("Você escolheu Pedra!! ${resultado}")
+            userChooseStr = getString(R.string.stone)
+            userChoose = STONE
+            cpuChoose()
+            result()
             ivPapel.setVisibility(View.GONE)
             ivTesoura.setVisibility(View.GONE)
         }
 
         ivPapel.setOnClickListener{
-            if (randomStr == "Pedra") {
-                resultado = vitoria
-                ivPadrao.setImageResource((R.drawable.pedra))
-            }
-            if (randomStr == "Papel") {
-                resultado = empate
-                ivPadrao.setImageResource((R.drawable.papel))
-            }
-            if (randomStr == "Tesoura") {
-                resultado = derrota
-                ivPadrao.setImageResource((R.drawable.tesoura))
-            }
-            tvTitle.setText("O CPU escolheu ${randomStr}")
-            tvBody.setText("Você escolheu Pedra!! ${resultado}")
+            userChooseStr = getString(R.string.stone)
+            userChoose = PAPER
+            cpuChoose()
+            result()
             ivPedra.setVisibility(View.GONE)
             ivTesoura.setVisibility(View.GONE)
         }
 
         ivTesoura.setOnClickListener{
-            if (randomStr == "Pedra") {
-                resultado = derrota
-                ivPadrao.setImageResource((R.drawable.pedra))
-            }
-            if (randomStr == "Papel") {
-                resultado = vitoria
-                ivPadrao.setImageResource((R.drawable.papel))
-            }
-            if (randomStr == "Tesoura") {
-                resultado = empate
-                ivPadrao.setImageResource((R.drawable.tesoura))
-            }
-            tvTitle.setText("O CPU escolheu ${randomStr}")
-            tvBody.setText("Você escolheu Pedra!! ${resultado}")
+            userChooseStr = getString(R.string.scissor)
+            userChoose = SCISSOR
+            cpuChoose()
+            result()
             ivPapel.setVisibility(View.GONE)
             ivPedra.setVisibility(View.GONE)
         }
     }
+
+    private fun generateRandomNumber() = Random.nextInt(3)
+
+    private fun cpuChoose() {
+        when (generateRandomNumber()) {
+            STONE -> {cpuChoose=STONE; cpuChooseStr=getString(R.string.stone); ivPadrao.setImageResource((R.drawable.pedra))}
+            PAPER -> {cpuChoose=PAPER; cpuChooseStr=getString(R.string.paper); ivPadrao.setImageResource((R.drawable.papel))}
+            SCISSOR -> {cpuChoose=SCISSOR; cpuChooseStr=getString(R.string.scissor); ivPadrao.setImageResource((R.drawable.tesoura))}
+        }
+        tvTitle.setText(getString(R.string.cpu_choose,cpuChooseStr))
+    }
+
+    private fun result() {
+        when{
+            userChoose == STONE && cpuChoose == STONE -> tvBody.setText(R.string.draw)
+            userChoose == STONE && cpuChoose == PAPER -> tvBody.setText(R.string.lose)
+            userChoose == STONE && cpuChoose == SCISSOR -> tvBody.setText(R.string.win)
+            userChoose == PAPER && cpuChoose == STONE -> tvBody.setText(R.string.win)
+            userChoose == PAPER && cpuChoose == PAPER -> tvBody.setText(R.string.draw)
+            userChoose == PAPER && cpuChoose == SCISSOR -> tvBody.setText(R.string.lose)
+            userChoose == SCISSOR && cpuChoose == STONE -> tvBody.setText(R.string.lose)
+            userChoose == SCISSOR && cpuChoose == PAPER -> tvBody.setText(R.string.win)
+            userChoose == SCISSOR && cpuChoose == SCISSOR -> tvBody.setText(R.string.draw)
+        }
+    }
+
+    companion object {
+        const val STONE = 0
+        const val PAPER = 1
+        const val SCISSOR = 2
+    }
+
 }
